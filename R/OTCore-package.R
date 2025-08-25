fb5ef7c20d2b3ca2068c8f268decb2f432ea0c1d
Tsung-Heng Tsai <tsai.tsungheng@gmail.com>
2025-08-25 13:18:51 -0400

    Package import

---
+++ R/OTCore-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr arrange
+#' @importFrom dplyr count
+#' @importFrom dplyr distinct
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom dplyr slice
+#' @importFrom rlang quos
+#' @importFrom tibble tibble
+#' @importFrom tidyr nest
+#' @importFrom tidyr unnest
+## usethis namespace: end
+NULL
